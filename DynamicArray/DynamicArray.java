9b5be30fd5b3f1a5a3534d77329a5b9ebd31007c
LD <ldaneliee33@gmail.com>
2025-06-06 04:36:36 +0800

    Finish product dynamic array

--- DynamicArray/DynamicArray.java
+++ DynamicArray/DynamicArray.java
@@ -1,14 +1,14 @@
 package DynamicArray;
 
 public class DynamicArray{
-    private int size;
-    private int capacity;
+    private int size = 0;
+    private int capacity = 10;
     private Object[] array;
 
     DynamicArray(){
         this.array = new Object[capacity];
     }
-    DynamicArray(Object array, int capacity){
+    DynamicArray(int capacity){
         this.capacity = capacity;
         this.array = new Object[capacity];
     }
@@ -16,20 +16,91 @@ public class DynamicArray{
     int capacity(){ return capacity; }
     int size(){ return size; }
 
-    void add(){
+    // For saving memory
+    void resize(){
 
-    }
-    void remove(){
+        System.out.println("This works!");
+        int newCapacity = 0;
+        
+        if (size >= capacity) {
+            newCapacity = capacity * 2;
+        } else if (size <= capacity / 3 && capacity > 1) {
+            newCapacity = capacity / 2;
+        }
 
-    }
-    void insert(){
+        Object[] newArray = new Object[newCapacity];
 
+        for(int i = 0; i < size; i++){
+            newArray[i] = array[i];
+        }
+        capacity = newCapacity;
+        array = newArray;
     }
-    void get(){
+
+    void push_back(Object element){
+
+        if (size >= capacity){ resize(); }
+        array[size] = element;
+        size++;
     }
-    String string(){
-        String string = "hello";
-        return string;
+
+    // Inserts the element at certain index
+    void insert(int index, Object element){
+
+        if (size >= capacity) { resize(); }
+
+        // Shifting element to the right
+        for(int i = size - 1; i >= index; i--){
+            array[i + 1] = array[i];
+        } 
+        array[index] = element;
+        size++;
+    }
+
+    void remove(Object element){
+
+        for(int i = 0; i < size; i++){
+            if(array[i] == element){
+                for(int j = i; j < size - 1; j++){
+                    array[j] = array[j + 1];
+                }
+                size--;
+                i--;
+            }
+        }
+        if(size < (int)capacity / 3){ resize(); }
+    }
+    void remove(int index){ 
+
+        for(int i = index; i < size; i++){
+            array[i] = array[i + 1];
+        } 
+        array[size - 1] = null;
+        size--;
+        if(size < (int)capacity / 3){ resize(); }
+    }
+
+    int get(Object element){
+        for(int i = 0; i < size; i++){
+            if(element == array[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Customizing the default string value
+    @Override
+    public String toString(){
+
+        String string = "[";
+
+        for(int i = 0; i < size; i++){
+            string += array[i] + ", ";
+        }
+        string = string.substring(0, string.length() - 2);
+        
+        return string + "]";
     }
 
 }
--- DynamicArray/Main.java
+++ DynamicArray/Main.java
@@ -1,5 +1,30 @@
 package DynamicArray;
 
 public class Main{
-    
+    public static void main(String[] args) {
+
+        DynamicArray example = new DynamicArray();
+ 
+        example.push_back("Abcd"); 
+        example.push_back("B"); 
+        example.push_back("C");
+        example.push_back("J"); 
+        example.push_back("D"); 
+        example.push_back("E"); 
+        example.push_back("G"); 
+        example.push_back(2);
+        example.insert(6, "J"); 
+        example.insert(8, "J"); 
+
+
+        System.out.println("Size: " + example.size());
+        System.out.println("Capacity: " + example.capacity());
+        System.out.println("Find element \"G\" at index: " + example.get("G"));
+        System.out.println(example + "\n");
+
+        System.out.println("Removing: J");
+        example.remove("J");
+        System.out.println("Size after removal: " + example.size());
+        System.out.println(example);
+    }
 }
