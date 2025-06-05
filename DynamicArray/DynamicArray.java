df621c05bbe51d24c57177e54a5761dbc13348bc
LD <ldaneliee33@gmail.com>
2025-06-05 23:38:21 +0800

    addingDynamicArray

---
+++ DynamicArray/DynamicArray.java
@@ -0,0 +1,33 @@
+public class DynamicArray{
+    private int size;
+    private int capacity;
+    private Object[] array;
+
+    DynamicArray(){
+        this.array = new Object[capacity];
+    }
+    DynamicArray(Object array, int capacity){
+        this.capacity = capacity;
+        this.array = new Object[capacity];
+    }
+
+    int capacity(){ return capacity; }
+    int size(){ return size; }
+
+    void add(){
+
+    }
+    void remove(){
+
+    }
+    void insert(){
+
+    }
+    void get(){
+    }
+    String string(){
+        String string = "hello";
+        return string;
+    }
+
+}
